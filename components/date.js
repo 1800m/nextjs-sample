abaedd9cc33c78db64718b38204e0daa49060db2
1800m <type.cm.38@gmail.com>
2023-07-27 14:25:08 +0900

    propertyを修正

--- components/date.js
+++ components/date.js
@@ -2,5 +2,5 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ date_string }) {
   const date = parseISO(date_string);
-  return <time date_time={date_string}>{format(date, "LLLL d, yyyy")}</time>;
+  return <time dateTime={date_string}>{format(date, "LLLL d, yyyy")}</time>;
 }
